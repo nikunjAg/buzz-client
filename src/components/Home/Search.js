d1f57a8af14b6ac0c088139f0cc934772c9f4521
Nikunj Aggarwal <nikunj.aggarwal@tothenew.com>
2021-12-13 18:01:35 +0530

    Added Search Component

--- src/components/Home/Search.js
+++ src/components/Home/Search.js
@@ -1,9 +1,35 @@
 import React from "react";
 
 import classes from "./Search.module.css";
+import Card from "../UI/Card/Card";
+import profileImage from "../../assets/profile.jpg";
 
 const Search = (props) => {
-	return <div className={classes.Search}></div>;
+	return (
+		<Card className={classes.Search}>
+			<div
+				className={classes.Avatar}
+				style={{
+					backgroundImage: `url(${profileImage})`,
+					backgroundPosition: "center",
+					backgroundSize: "cover",
+					backgroundRepeat: "no-repeat",
+				}}
+			></div>
+			<input
+				type="text"
+				name="search"
+				className={classes.SearchBar}
+				placeholder="Start a post..."
+			/>
+			<input
+				type="image"
+				name="post-image"
+				className={classes.ImagePicker}
+				alt="Post Image"
+			/>
+		</Card>
+	);
 };
 
 export default Search;

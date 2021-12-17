e387e55d41c4ae5263d861ccd80c6585174130d1
Nikunj Aggarwal <nikunj.aggarwal@tothenew.com>
2021-12-17 18:03:04 +0530

    Created Add Comment component

---
+++ src/components/Home/Post/AddComment.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import classes from "./AddComment.module.css";
+import Avatar from "../../UI/Avatar/Avatar";
+
+const AddComment = () => {
+	const profileImage = useSelector((state) => state.user.profileImage);
+
+	return (
+		<div className={classes.addComment}>
+			<Avatar
+				className={classes.avatar}
+				src={profileImage}
+				alt="Profile Image"
+			/>
+			<input
+				placeholder="Write a comment..."
+				name="comment"
+				className={classes.commentInput}
+			/>
+		</div>
+	);
+};
+
+export default AddComment;

4f430827655b52a57128f21803d2f6a57b3d262f
Nikunj Aggarwal <nikunj.aggarwal@tothenew.com>
2021-12-18 16:57:17 +0530

    Updated the Notifications component styling

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -3,49 +3,57 @@ import Avatar from "../UI/Avatar/Avatar";
 
 import classes from "./Notifications.module.css";
 import profileImage from "../../assets/profile.jpg";
+import ProfileCard from "../Home/ShortProfile";
+import Card from "../UI/Card/Card";
 
 const Notifications = (props) => {
 	return (
 		<div className={classes.notifications}>
-			<h1>Notifications</h1>
-			<ul className={classes.notificationsList}>
-				<li className={`${classes.notificationItem} ${classes.unread}`}>
-					<Avatar
-						className={classes.avatar}
-						src={profileImage}
-						alt="Sender Image"
-					/>
-					<div className={classes.message}>
-						Nikunj Aggarwal sent a friend request
-					</div>
-					<div className={classes.actions}>
-						<div className={`${classes.accept} ${classes.action}`}>
-							<span class="material-icons-outlined">done</span>
+			<div className={classes.sideCard}>
+				<ProfileCard />
+			</div>
+
+			<Card className={classes.invitations}>
+				<h3>Invitations</h3>
+				<ul className={classes.notificationsList}>
+					<li
+						className={`${classes.notificationItem} ${classes.unread}`}
+					>
+						<Avatar
+							className={classes.avatar}
+							src={profileImage}
+							alt="Sender Image"
+						/>
+						<div className={classes.sender}>
+							<div className={classes.name}>Nikunj Aggarwal</div>
+							<div className={classes.shortBio}>
+								Newly Recruit at TTN
+							</div>
 						</div>
-						<div className={`${classes.decline} ${classes.action}`}>
-							<span class="material-icons-outlined">close</span>
+						<div className={classes.actions}>
+							<div className={classes.accept}>Accept</div>
+							<div className={classes.decline}>Ignore</div>
 						</div>
-					</div>
-				</li>
-				<li className={classes.notificationItem}>
-					<Avatar
-						className={classes.avatar}
-						src={profileImage}
-						alt="Sender Image"
-					/>
-					<div className={classes.message}>
-						Nikunj Aggarwal sent a friend request
-					</div>
-					<div className={classes.actions}>
-						<div className={`${classes.accept} ${classes.action}`}>
-							<span class="material-icons-outlined">done</span>
+					</li>
+					<li className={`${classes.notificationItem}`}>
+						<Avatar
+							className={classes.avatar}
+							src={profileImage}
+							alt="Sender Image"
+						/>
+						<div className={classes.sender}>
+							<div className={classes.name}>Nikunj Aggarwal</div>
+							<div className={classes.shortBio}>
+								Newly Recruit at TTN
+							</div>
 						</div>
-						<div className={`${classes.decline} ${classes.action}`}>
-							<span class="material-icons-outlined">close</span>
+						<div className={classes.actions}>
+							<div className={classes.accept}>Accept</div>
+							<div className={classes.decline}>Ignore</div>
 						</div>
-					</div>
-				</li>
-			</ul>
+					</li>
+				</ul>
+			</Card>
 		</div>
 	);
 };

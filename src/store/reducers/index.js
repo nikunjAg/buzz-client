067052717522cabd279f5c3308ab7e3219f6f271
Nikunj Aggarwal <nikunj.aggarwal@tothenew.com>
2021-12-18 17:01:53 +0530

    Added the Notifications reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import authReducer from "./auth.reducer";
 import userReducer from "./user.reducer";
+import notificationsReducer from "./notifications.reducer";
 
 const rootReducer = combineReducers({
 	auth: authReducer,
 	user: userReducer,
+	notifications: notificationsReducer,
 });
 
 export default rootReducer;
---
+++ src/store/reducers/notifications.reducer.js
@@ -0,0 +1,55 @@
+import {
+	FETCH_UNREAD_NOTIFICATIONS_STARTED,
+	FETCH_UNREAD_NOTIFICATIONS_SUCCESS,
+	FETCH_UNREAD_NOTIFICATIONS_FAILED,
+	FETCH_NOTIFICATIONS_STARTED,
+	FETCH_NOTIFICATIONS_SUCCESS,
+	FETCH_NOTIFICATIONS_FAILED,
+} from "../actions/notifications.action";
+
+const initialState = {
+	unreadNotifications: 0,
+	invitations: [],
+	loading: false,
+	error: null,
+};
+
+const userReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case FETCH_UNREAD_NOTIFICATIONS_STARTED:
+			return {
+				...state,
+				loading: true,
+				error: null,
+				unreadNotifications: 0,
+			};
+		case FETCH_UNREAD_NOTIFICATIONS_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				unreadNotifications: action.notifications,
+			};
+		case FETCH_UNREAD_NOTIFICATIONS_FAILED:
+			return { ...state, loading: false, error: action.error_message };
+
+		case FETCH_NOTIFICATIONS_STARTED:
+			return {
+				...state,
+				loading: true,
+				error: null,
+				invitations: 0,
+			};
+		case FETCH_NOTIFICATIONS_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				invitations: action.notifications,
+			};
+		case FETCH_NOTIFICATIONS_FAILED:
+			return { ...state, loading: false, error: action.error_message };
+		default:
+			return state;
+	}
+};
+
+export default userReducer;

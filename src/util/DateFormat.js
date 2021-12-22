57108f04d7dde7ada513641b11f8bb2f6912f2d8
Nikunj Aggarwal <nikunj.aggarwal@tothenew.com>
2021-12-22 16:32:22 +0530

    Corrected the DateFormat bug

--- src/util/DateFormat.js
+++ src/util/DateFormat.js
@@ -1,5 +1,5 @@
-const formatDate = (dateStr) => {
-	const date = new Date(dateStr || null);
+const formatDate = (dateVal) => {
+	const date = new Date(dateVal.toString() || null);
 	return date.toLocaleDateString("en-US", {
 		month: "long",
 		day: "numeric",
